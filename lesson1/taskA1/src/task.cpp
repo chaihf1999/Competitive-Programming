bc21808fb796f649a2298beced0aaa07fe82e242
chaihf1999 <chaihf@foxmail.com>
2022-12-26 19:05:39 +0800

    convolution_mod

--- lesson1/taskA1/src/task.cpp
+++ lesson1/taskA1/src/task.cpp
@@ -1,8 +1,63 @@
 #include <bits/stdc++.h>
-// fast_io
+// convolution_mod
 using i64 = int64_t;
 using u32 = uint32_t;
 using u64 = uint64_t;
+const int N = 1 << 20;
+const int P = 998244353;
+struct mint {
+    int x;
+    constexpr mint(int x = 0) : x(x) {}
+    mint operator+(mint o) const { return x + o.x < P ? x + o.x : x + o.x - P; }
+    mint operator-(mint o) const { return x - o.x < 0 ? x - o.x + P : x - o.x; }
+    mint operator*(mint o) const { return int(u64(x) * o.x % P); }
+    mint operator-() const { return x ? P - x : 0; }
+    void operator+=(mint o) { *this = *this + o; }
+    void operator-=(mint o) { *this = *this - o; }
+    void operator*=(mint o) { *this = *this * o; }
+    mint pow(auto k) const {
+        mint a = x;
+        mint b = 1;
+        for (; k > 0; k /= 2) {
+            if (k & 1)
+                b *= a;
+            a *= a;
+        }
+        return b;
+    }
+};
+mint w[N];
+void __attribute((constructor)) init() {
+    w[N / 2] = 1;
+    mint g = mint(3).pow(P / N);
+    for (int i = N / 2 + 1; i < N; ++i) w[i] = w[i - 1] * g;
+    for (int i = N / 2 - 1; i > 0; --i) w[i] = w[i * 2];
+}
+void fft(mint f[], int n) {
+    for (int k = n / 2; k; k /= 2)
+        for (int i = 0; i < n; i += k + k)
+            for (int j = 0; j < k; ++j) {
+                mint x = f[i + j];
+                mint y = f[i + j + k];
+                f[i + j] = x + y;
+                f[i + j + k] = (x - y) * w[j + k];
+            }
+}
+void ift(mint f[], int n) {
+    for (int k = 1; k < n; k *= 2)
+        for (int i = 0; i < n; i += k + k)
+            for (int j = 0; j < k; ++j) {
+                mint x = f[i + j];
+                mint y = f[i + j + k] * w[j + k];
+                f[i + j] = x + y;
+                f[i + j + k] = x - y;
+            }
+    mint inv = P - (P - 1) / n;
+    std::reverse(f + 1, f + n);
+    for (int i = 0; i < n; ++i) f[i] *= inv;
+}
+mint a[N];
+mint b[N];
 int main() {
 #ifdef LOCAL
     auto flush = [&]() {};
@@ -49,6 +104,15 @@ int main() {
         ptrO += end - ptr;
     };
 #endif
-    for (auto i = ii(); i > 0; --i) oo(ii() + ii());
+    int n = ii();
+    int m = ii();
+    int o = 2 << std::__lg(n + m - 1);
+    for (int i = 0; i < n; ++i) a[i] = ii();
+    for (int i = 0; i < m; ++i) b[i] = ii();
+    fft(a, o);
+    fft(b, o);
+    for (int i = 0; i < o; ++i) a[i] *= b[i];
+    ift(a, o);
+    for (int i = 0; i < n + m - 1; ++i) oo(a[i].x);
     flush();
 }
