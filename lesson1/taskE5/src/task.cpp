f3c92220363d605494017060ed97743d14d8962d
chaihf1999 <chaihf@foxmail.com>
2022-12-27 12:10:05 +0800

    subset_convolution

--- lesson1/taskE5/src/task.cpp
+++ lesson1/taskE5/src/task.cpp
@@ -1,8 +1,46 @@
 #include <bits/stdc++.h>
-// fast_io
+// subset_convolution
 using i64 = int64_t;
 using u32 = uint32_t;
 using u64 = uint64_t;
+const int N = 1 << 20;
+const int P = 998244353;
+struct mint {
+    int x;
+    constexpr mint(int x = 0) : x(x) {}
+    mint operator+(mint o) const { return x + o.x < P ? x + o.x : x + o.x - P; }
+    mint operator-(mint o) const { return x - o.x < 0 ? x - o.x + P : x - o.x; }
+    mint operator*(mint o) const { return int(u64(x) * o.x % P); }
+    mint operator-() const { return x ? P - x : 0; }
+    void operator+=(mint o) { *this = *this + o; }
+    void operator-=(mint o) { *this = *this - o; }
+    void operator*=(mint o) { *this = *this * o; }
+    mint pow(auto k) const {
+        mint a = x;
+        mint b = 1;
+        for (; k > 0; k /= 2) {
+            if (k & 1)
+                b *= a;
+            a *= a;
+        }
+        return b;
+    }
+};
+void fft(mint f[], int n) {
+    for (int k = 1; k < n; k *= 2)
+        for (int i = 0; i < n; i += k + k)
+            for (int j = 0; j < k; ++j)
+                f[i + j + k] += f[i + j];
+}
+void ift(mint f[], int n) {
+    for (int k = 1; k < n; k *= 2)
+        for (int i = 0; i < n; i += k + k)
+            for (int j = 0; j < k; ++j)
+                f[i + j + k] -= f[i + j];
+}
+mint a[21][N];
+mint b[21][N];
+mint c[21][N];
 int main() {
 #ifdef LOCAL
     auto flush = [&]() {};
@@ -49,6 +87,17 @@ int main() {
         ptrO += end - ptr;
     };
 #endif
-    for (auto i = ii(); i > 0; --i) oo(ii() + ii());
+    int m = ii();
+    int n = 1 << m++;
+    for (int i = 0; i < n; ++i) a[__builtin_popcount(i)][i] = ii();
+    for (int i = 0; i < n; ++i) b[__builtin_popcount(i)][i] = ii();
+    for (int i = 0; i < m; ++i) fft(a[i], n);
+    for (int i = 0; i < m; ++i) fft(b[i], n);
+    for (int i = 0; i < m; ++i)
+        for (int j = 0; j <= i; ++j)
+            for (int k = 0; k < n; ++k)
+                c[i][k] += a[j][k] * b[i - j][k];
+    for (int i = 0; i < m; ++i) ift(c[i], n);
+    for (int i = 0; i < n; ++i) oo(c[__builtin_popcount(i)][i].x);
     flush();
 }
