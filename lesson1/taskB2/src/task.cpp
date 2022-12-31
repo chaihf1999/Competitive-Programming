57e31693b251f8a28165add6f1765e2df197c751
chai2333 <chaihf@foxmail.com>
2022-12-31 17:14:24 +0800

    predecessor_problem

--- lesson1/taskB2/src/task.cpp
+++ lesson1/taskB2/src/task.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-// fast_io
+// predecessor_problem
 using i64 = int64_t;
 using u32 = uint32_t;
 using u64 = uint64_t;
@@ -10,6 +10,11 @@ public:
         int x;
         std::cin >> x;
         return x;
+    }
+    inline auto operator!() {
+        char x;
+        std::cin >> x;
+        return x == '1';
     }
     inline void operator()(auto x, char c = ' ') {
         std::cout << x << c << std::flush;
@@ -36,8 +41,13 @@ public:
         int n{};
         for (; *ptrI < 48; ++ptrI) n = *ptrI == 45;
         for (; *ptrI > 47; ++ptrI) x = x * 10 + *ptrI - 48;
+        ++ptrI;
         return n ? -x : +x;
     };
+    inline auto operator!() {
+        if (endI - ptrI < 32) load();
+        return *ptrI++ == '1';
+    }
     inline void operator()(auto x, char c = ' ') {
         if (endO - ptrO < 32) flush();
         if (x < 0) x = -x, *ptrO++ = '-';
@@ -55,6 +65,73 @@ public:
     ~IO() { flush(); }
 } io;
 #endif
+const int N = 1 << 24;
+std::bitset<N * 2> vec;
+void insert(int k) {
+    k += N;
+    for (; k > 0 && !vec[k]; k /= 2)
+        vec.set(k);
+}
+void remove(int k) {
+    k += N;
+    if (!vec[k]) return;
+    vec.reset(k);
+    for (; k > 1 && !vec[k ^ 1]; k /= 2)
+        vec.reset(k / 2);
+}
+int ge(int k) {
+    k += N;
+    if (vec[k]) return k - N;
+    for (; k > 0; k /= 2) {
+        if (!(k & 1) && vec[k ^ 1]) {
+            k ^= 1;
+            while (k < N)
+                k = vec[k * 2] ? k * 2 : k * 2 + 1;
+            return k - N;
+        }
+    }
+    return -1;
+}
+int le(int k) {
+    k += N;
+    if (vec[k]) return k - N;
+    for (; k > 0; k /= 2) {
+        if (+(k & 1) && vec[k ^ 1]) {
+            k ^= 1;
+            while (k < N)
+                k = vec[k * 2 + 1] ? k * 2 + 1 : k * 2;
+            return k - N;
+        }
+    }
+    return -1;
+}
 int main() {
-    for (auto i = io(); i > 0; --i) io(io() + io());
+    int n = io();
+    int m = io();
+    for (int i = 0; i < n; ++i) vec[N + i] = !io;
+    for (int i = N + n; i > 1; i -= 2) vec[i / 2] = vec[i] || vec[i ^ 1];
+    for (int i = 0; i < m; ++i) {
+        switch (io()) {
+            case 0: { // insert
+                insert(io());
+                break;
+            }
+            case 1: { // remove
+                remove(io());
+                break;
+            }
+            case 2: { // contain
+                io(int(vec[N + io()]));
+                break;
+            }
+            case 3: { // ge
+                io(ge(io()));
+                break;
+            }
+            case 4: { // le
+                io(le(io()));
+                break;
+            }
+        }
+    }
 }
