52b06c0196c389b4b39a00f36414970e9a970bfd
chaihf1999 <chaihf@foxmail.com>
2023-01-05 21:07:24 +0800

    point_set_range_composite

--- lesson1/taskG7/src/task.cpp
+++ lesson1/taskG7/src/task.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-// fast_io
+// point_set_range_composite
 using i64 = int64_t;
 using u32 = uint32_t;
 using u64 = uint64_t;
@@ -55,6 +55,45 @@ public:
     ~IO() { flush(); }
 } io;
 #endif
+const int N = 500100;
+const int P = 998244353;
+struct mint {
+    int x;
+    constexpr mint(int x = 0) : x(x) {}
+    mint operator+(mint o) const { return x + o.x < P ? x + o.x : x + o.x - P; }
+    mint operator-(mint o) const { return x - o.x < 0 ? x - o.x + P : x - o.x; }
+    mint operator*(mint o) const { return int(u64(x) * o.x % P); }
+    mint operator-() const { return x ? P - x : 0; }
+    void operator+=(mint o) { *this = *this + o; }
+    void operator-=(mint o) { *this = *this - o; }
+    void operator*=(mint o) { *this = *this * o; }
+};
+struct Node {
+    mint a;
+    mint b;
+    Node operator+(Node o) const { return {o.a * a, o.a * b + o.b}; }
+};
+Node tree[N + N];
 int main() {
-    for (auto i = io(); i > 0; --i) io(io() + io());
+    int n = io();
+    int m = io();
+    for (int i = 0; i < n; ++i) tree[n + i] = {io(), io()};
+    for (int i = n - 1; i; --i) tree[i] = tree[i * 2] + tree[i * 2 + 1];
+    for (int i = 0; i < m; ++i) {
+        if (io() == 0) {
+            int k = n + io();
+            tree[k] = {io(), io()};
+            for (k /= 2; k > 0; k /= 2) tree[k] = tree[k * 2] + tree[k * 2 + 1];
+        } else {
+            int l = n + io() - 1;
+            int r = n + io();
+            Node ansL = {0, io()};
+            Node ansR = {1, 0};
+            for (; l != r - 1; l /= 2, r /= 2) {
+                if (~l & 1) ansL = ansL + tree[l ^ 1];
+                if (+r & 1) ansR = tree[r ^ 1] + ansR;
+            }
+            io((ansL + ansR).b.x);
+        }
+    }
 }
