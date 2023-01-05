f6d00819b4e1b3f25f5f8ce599d654b094c5bb20
chaihf1999 <chaihf@foxmail.com>
2023-01-05 19:49:30 +0800

    static_rmq

--- lesson1/taskE5/src/task.cpp
+++ lesson1/taskE5/src/task.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-// fast_io
+// static_rmq
 using i64 = int64_t;
 using u32 = uint32_t;
 using u64 = uint64_t;
@@ -55,6 +55,36 @@ public:
     ~IO() { flush(); }
 } io;
 #endif
+const int K = 5;
+const int N = 1 << 19;
+const int M = 1 << 14;
+const int mask = (1 << K) - 1;
+int val[N];
+int pre[N];
+int suf[N];
+int min[14][M];
 int main() {
-    for (auto i = io(); i > 0; --i) io(io() + io());
+    int n = io();
+    int m = io();
+    int o = n >> K;
+    for (int i = 0; i < n; ++i) val[i] = io();
+    for (int i = 0; i < n; ++i) pre[i] = (i & mask) ? std::min(pre[i - 1], val[i]) : val[i];
+    for (int i = n; i > 0; --i) suf[i - 1] = (i & mask) ? std::min(suf[i], val[i - 1]) : val[i - 1];
+    for (int i = 0; i < o; ++i) min[0][i] = suf[i << K];
+    for (int k = 1; o >> k; ++k)
+        for (int i = (1 << k) - 1, j = (1 << (k - 1)) - 1; i < o; ++i, ++j)
+            min[k][i] = std::min(min[k - 1][i], min[k - 1][j]);
+    for (int i = 0; i < m; ++i) {
+        int l = io();
+        int r = io() - 1;
+        if (r - l < 64) {
+            io(*std::min_element(val + l, val + r + 1));
+        } else {
+            int L = (l >> K) + 1;
+            int R = (r >> K) - 1;
+            int k = std::__lg(R - L + 1);
+            int ans = std::min(std::min(suf[l], pre[r]), std::min(min[k][L + (1 << k) - 1], min[k][R]));
+            io(ans);
+        }
+    }
 }
